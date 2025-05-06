87eded3cddf53a35a14307c05cae88e20642e8fc
Aaron Brethorst <aaron@brethorsting.com>
2025-05-06 15:59:52 -0700

    Moves response generation functions into response.go

--- internal/models/references.go
+++ internal/models/references.go
@@ -1,7 +1,5 @@
 package models
 
-import "time"
-
 // ReferencesModel References model for related data
 type ReferencesModel struct {
 	Agencies   []AgencyReference `json:"agencies"`
@@ -21,38 +19,5 @@ func NewEmptyReferences() ReferencesModel {
 		StopTimes:  []interface{}{},
 		Stops:      []interface{}{},
 		Trips:      []interface{}{},
-	}
-}
-
-// NewOKResponse is a helper function that returns a successful response.
-func NewOKResponse(data interface{}) ResponseModel {
-	return NewResponse(200, data, "OK")
-}
-
-func NewListResponse(list interface{}, references ReferencesModel) ResponseModel {
-	data := map[string]interface{}{
-		"limitExceeded": false,
-		"list":          list,
-		"references":    references,
-	}
-	return NewOKResponse(data)
-}
-
-func NewEntryResponse(entry interface{}, references ReferencesModel) ResponseModel {
-	data := map[string]interface{}{
-		"entry":      entry,
-		"references": references,
-	}
-	return NewOKResponse(data)
-}
-
-// NewResponse Helper function to create a standard response
-func NewResponse(code int, data interface{}, text string) ResponseModel {
-	return ResponseModel{
-		Code:        code,
-		CurrentTime: time.Now().UnixNano() / int64(time.Millisecond),
-		Data:        data,
-		Text:        text,
-		Version:     2,
 	}
 }
