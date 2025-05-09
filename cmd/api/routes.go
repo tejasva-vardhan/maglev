3e15da6aa1a9cf30c674c2d267ef5baa8adaf95f
Aaron Brethorst <aaron@brethorsting.com>
2025-05-09 10:00:13 -0700

    Wraps all API endpoint handlers with API key validation middleware
    Fixes #24

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -2,25 +2,38 @@ package main
 
 import (
 	"net/http"
+	"net/http/pprof"
 )
+
+type handlerFunc func(w http.ResponseWriter, r *http.Request)
+
+func validateAPIKey(app *application, finalHandler handlerFunc) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if app.requestHasInvalidAPIKey(r) {
+			app.invalidAPIKeyResponse(w, r)
+			return
+		}
+		finalHandler(w, r)
+	})
+}
+
+func registerPprofHandlers(mux *http.ServeMux) { // nolint:unused
+	// Register pprof handlers
+	// import "net/http/pprof"
+	// Tutorial: https://medium.com/@rahul.fiem/application-performance-optimization-how-to-effectively-analyze-and-optimize-pprof-cpu-profiles-95280b2f5bfb
+	mux.HandleFunc("/debug/pprof/", pprof.Index)
+	mux.HandleFunc("/debug/pprof/cmdline", pprof.Cmdline)
+	mux.HandleFunc("/debug/pprof/profile", pprof.Profile)
+	mux.HandleFunc("/debug/pprof/symbol", pprof.Symbol)
+	mux.HandleFunc("/debug/pprof/trace", pprof.Trace)
+}
 
 func (app *application) routes() http.Handler {
 	mux := http.NewServeMux()
-	mux.HandleFunc("GET /api/where/agencies-with-coverage.json", app.agenciesWithCoverageHandler)
-	mux.HandleFunc("GET /api/where/agency/{id}", app.agencyHandler)
-	mux.HandleFunc("GET /api/where/current-time.json", app.currentTimeHandler)
-	mux.HandleFunc("GET /api/where/routes-for-agency/{id}", app.routesForAgencyHandler)
-
-	// Register pprof handlers - https://medium.com/@rahul.fiem/application-performance-optimization-how-to-effectively-analyze-and-optimize-pprof-cpu-profiles-95280b2f5bfb
-	// 	"net/http/pprof"
-	// Register pprof handlers
-	// tutorial: https://medium.com/@rahul.fiem/application-performance-optimization-how-to-effectively-analyze-and-optimize-pprof-cpu-profiles-95280b2f5bfb
-	// import "net/http/pprof"
-	//mux.HandleFunc("/debug/pprof/", pprof.Index)
-	//mux.HandleFunc("/debug/pprof/cmdline", pprof.Cmdline)
-	//mux.HandleFunc("/debug/pprof/profile", pprof.Profile)
-	//mux.HandleFunc("/debug/pprof/symbol", pprof.Symbol)
-	//mux.HandleFunc("/debug/pprof/trace", pprof.Trace)
+	mux.Handle("GET /api/where/agencies-with-coverage.json", validateAPIKey(app, app.agenciesWithCoverageHandler))
+	mux.Handle("GET /api/where/agency/{id}", validateAPIKey(app, app.agencyHandler))
+	mux.Handle("GET /api/where/current-time.json", validateAPIKey(app, app.currentTimeHandler))
+	mux.Handle("GET /api/where/routes-for-agency/{id}", validateAPIKey(app, app.routesForAgencyHandler))
 
 	return mux
 }
