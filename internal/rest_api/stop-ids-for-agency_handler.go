5ecc7cabab3bb8df6fbaf3bd6ce45ddc9ed8f86e
Ahmedhossamdev <ahmedhossamdev1@gmail.com>
2025-06-01 19:00:51 +0300

    Add handler for retrieving stop IDs by agency

---
+++ internal/rest_api/stop-ids-for-agency_handler.go
@@ -0,0 +1,39 @@
+package restapi
+
+import (
+	"context"
+	"net/http"
+
+	"maglev.onebusaway.org/internal/models"
+	"maglev.onebusaway.org/internal/utils"
+)
+
+func (api *RestAPI) stopIDsForAgencyHandler(w http.ResponseWriter, r *http.Request) {
+
+	id := utils.ExtractIDFromParams(r)
+
+	agency := api.GtfsManager.FindAgency(id)
+
+	if agency == nil {
+		api.sendNotFound(w, r)
+		return
+	}
+
+	ctx := context.Background()
+
+	stopIDs, err := api.GtfsManager.GtfsDB.Queries.GetStopIDsForAgency(ctx, id)
+	if err != nil {
+
+	}
+	if stopIDs == nil {
+		api.sendNotFound(w, r)
+		return
+	}
+	response := make([]string, 0, len(stopIDs))
+	for _, stopID := range stopIDs {
+		response = append(response, utils.FormCombinedID(id, stopID))
+	}
+
+	api.sendResponse(w, r, models.NewListResponse(response, models.NewEmptyReferences()))
+
+}
