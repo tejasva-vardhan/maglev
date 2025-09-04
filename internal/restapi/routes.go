10329e5a6a584d34b403885d306bc1c2f6982b11
Amr Hossam <amrhossam.dev@gmail.com>
2025-09-05 01:00:25 +0300

    feat: add trips for route endpoint

--- internal/restapi/routes.go
+++ internal/restapi/routes.go
@@ -71,6 +71,7 @@ func (api *RestAPI) SetRoutes(mux *http.ServeMux) {
 	mux.Handle("GET /api/where/trip-for-vehicle/{id}", rateLimitAndValidateAPIKey(api, api.tripForVehicleHandler))
 	mux.Handle("GET /api/where/trips-for-location.json", rateLimitAndValidateAPIKey(api, api.tripsForLocationHandler))
 	mux.Handle("GET /api/where/arrival-and-departure-for-stop/{id}", rateLimitAndValidateAPIKey(api, api.arrivalAndDepartureForStopHandler))
+	mux.Handle("GET /api/where/trips-for-route/{id}", rateLimitAndValidateAPIKey(api, api.tripsForRouteHandler))
 }
 
 // SetupAPIRoutes creates and configures the API router with all middleware applied globally
