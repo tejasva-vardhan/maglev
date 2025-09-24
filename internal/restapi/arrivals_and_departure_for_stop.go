83e3a887dadd50e5c3dd96e197a91f3043c17396
Amr Hossam <amrhossam.dev@gmail.com>
2025-09-24 03:26:19 +0300

    feat: implement arrivals and departures handler for specific stop

---
+++ internal/restapi/arrivals_and_departure_for_stop.go
@@ -0,0 +1,290 @@
+package restapi
+
+import (
+	"maglev.onebusaway.org/gtfsdb"
+	"maglev.onebusaway.org/internal/models"
+	"maglev.onebusaway.org/internal/utils"
+	"net/http"
+	"strconv"
+	"time"
+)
+
+func (api *RestAPI) arrivalsAndDeparturesForStopHandler(w http.ResponseWriter, r *http.Request) {
+	stopID := utils.ExtractIDFromParams(r)
+	agencyID, stopCode, err := utils.ExtractAgencyIDAndCodeID(stopID)
+	if err != nil {
+		api.serverErrorResponse(w, r, err)
+		return
+	}
+
+	ctx := r.Context()
+	params := ArrivalAndDepartureParams{
+		MinutesAfter:  35,
+		MinutesBefore: 5,
+	}
+
+	if minutesAfterStr := r.URL.Query().Get("minutesAfter"); minutesAfterStr != "" {
+		if minutesAfter, err := strconv.Atoi(minutesAfterStr); err == nil {
+			params.MinutesAfter = minutesAfter
+		}
+	}
+	if minutesBeforeStr := r.URL.Query().Get("minutesBefore"); minutesBeforeStr != "" {
+		if minutesBefore, err := strconv.Atoi(minutesBeforeStr); err == nil {
+			params.MinutesBefore = minutesBefore
+		}
+	}
+
+	var currentTime time.Time
+	if timeStr := r.URL.Query().Get("time"); timeStr != "" {
+		if timeMs, err := strconv.ParseInt(timeStr, 10, 64); err == nil {
+			currentTime = time.Unix(timeMs/1000, 0)
+		}
+	} else {
+		currentTime = time.Now()
+	}
+
+	_, err = api.GtfsManager.GtfsDB.Queries.GetStop(ctx, stopCode)
+	if err != nil {
+		api.sendNotFound(w, r)
+		return
+	}
+
+	agency, err := api.GtfsManager.GtfsDB.Queries.GetAgency(ctx, agencyID)
+	if err != nil {
+		api.serverErrorResponse(w, r, err)
+		return
+	}
+
+	loc, _ := time.LoadLocation(agency.Timezone)
+	currentTime = currentTime.In(loc)
+	windowStart := currentTime.Add(-time.Duration(params.MinutesBefore) * time.Minute)
+	windowEnd := currentTime.Add(time.Duration(params.MinutesAfter) * time.Minute)
+
+	windowStartNanos := convertToNanosSinceMidnight(windowStart)
+	windowEndNanos := convertToNanosSinceMidnight(windowEnd)
+
+	serviceDate := currentTime.Format("20060102")
+	activeServiceIDs, err := api.GtfsManager.GtfsDB.Queries.GetActiveServiceIDsForDate(ctx, serviceDate)
+	if err != nil {
+		api.serverErrorResponse(w, r, err)
+		return
+	}
+
+	if len(activeServiceIDs) == 0 {
+		arrivals := make([]models.ArrivalAndDeparture, 0)
+		references := models.NewEmptyReferences()
+
+		references.Agencies = append(references.Agencies, models.NewAgencyReference(
+			agency.ID, agency.Name, agency.Url, agency.Timezone, agency.Lang.String,
+			agency.Phone.String, agency.Email.String, agency.FareUrl.String, "", false,
+		))
+
+		response := models.NewArrivalsAndDepartureResponse(arrivals, references, []string{}, []string{}, stopID)
+		api.sendResponse(w, r, response)
+		return
+	}
+
+	// Get trips that serve this stop and are active today
+	activeTrips, err := api.GtfsManager.GtfsDB.Queries.GetTripsByServiceID(ctx, activeServiceIDs)
+	if err != nil {
+		api.serverErrorResponse(w, r, err)
+		return
+	}
+
+	activeTripIDs := make(map[string]bool)
+	for _, trip := range activeTrips {
+		activeTripIDs[trip.ID] = true
+	}
+
+	// Get all stop times for this stop within the time window
+	allStopTimes, err := api.GtfsManager.GtfsDB.Queries.GetStopTimesForStopInWindow(ctx, gtfsdb.GetStopTimesForStopInWindowParams{
+		StopID:           stopCode,
+		WindowStartNanos: windowStartNanos,
+		WindowEndNanos:   windowEndNanos,
+	})
+	if err != nil {
+		api.serverErrorResponse(w, r, err)
+		return
+	}
+
+	// Filter stop times to only include active trips
+	var stopTimes []struct {
+		TripID        string
+		ArrivalTime   int64
+		DepartureTime int64
+		StopSequence  int64
+		RouteID       string
+		ServiceID     string
+		TripHeadsign  string
+		BlockID       string
+	}
+
+	for _, st := range allStopTimes {
+		if activeTripIDs[st.TripID] {
+			stopTimes = append(stopTimes, struct {
+				TripID        string
+				ArrivalTime   int64
+				DepartureTime int64
+				StopSequence  int64
+				RouteID       string
+				ServiceID     string
+				TripHeadsign  string
+				BlockID       string
+			}{
+				TripID:        st.TripID,
+				ArrivalTime:   st.ArrivalTime,
+				DepartureTime: st.DepartureTime,
+				StopSequence:  st.StopSequence,
+				RouteID:       st.RouteID,
+				ServiceID:     st.ServiceID,
+				TripHeadsign:  st.TripHeadsign.String,
+				BlockID:       st.BlockID.String,
+			})
+		}
+	}
+
+	arrivals := make([]models.ArrivalAndDeparture, 0)
+	references := models.NewEmptyReferences()
+
+	references.Agencies = append(references.Agencies, models.NewAgencyReference(
+		agency.ID, agency.Name, agency.Url, agency.Timezone, agency.Lang.String,
+		agency.Phone.String, agency.Email.String, agency.FareUrl.String, "", false,
+	))
+
+	for _, st := range stopTimes {
+		route, err := api.GtfsManager.GtfsDB.Queries.GetRoute(ctx, st.RouteID)
+		if err != nil {
+			continue
+		}
+
+		loc, _ := time.LoadLocation(agency.Timezone)
+		serviceDateMillis := currentTime.UnixMilli()
+
+		serviceMidnight := time.Date(
+			currentTime.Year(),
+			currentTime.Month(),
+			currentTime.Day(),
+			0, 0, 0, 0,
+			loc,
+		)
+
+		scheduledArrivalTime := serviceMidnight.Add(time.Duration(st.ArrivalTime)).UnixMilli()
+		scheduledDepartureTime := serviceMidnight.Add(time.Duration(st.DepartureTime)).UnixMilli()
+
+		var (
+			predictedArrivalTime   = scheduledArrivalTime
+			predictedDepartureTime = scheduledDepartureTime
+			predicted              = false
+			vehicleID              string
+			tripStatus             *models.TripStatusForTripDetails
+			distanceFromStop       = 0.0
+			numberOfStopsAway      = 0
+		)
+
+		// Get real-time updates from GTFS-RT
+		vehicle := api.GtfsManager.GetVehicleForTrip(st.TripID)
+		if vehicle != nil && vehicle.Trip != nil {
+			vehicleID = vehicle.ID.ID
+
+			// Check if we have stop time updates for this trip
+			if len(vehicle.Trip.StopTimeUpdates) > 0 {
+				// Look for StopTimeUpdate that matches this stop
+				for _, stopTimeUpdate := range vehicle.Trip.StopTimeUpdates {
+					// Match by stop sequence or stop ID
+					if (stopTimeUpdate.StopSequence != nil && int64(*stopTimeUpdate.StopSequence) == st.StopSequence) ||
+						(stopTimeUpdate.StopID != nil && *stopTimeUpdate.StopID == stopCode) {
+
+						predicted = true
+
+						// Update predicted times from GTFS-RT
+						if stopTimeUpdate.Arrival != nil && stopTimeUpdate.Arrival.Time != nil {
+							predictedArrivalTime = stopTimeUpdate.Arrival.Time.Unix() * 1000
+						} else if stopTimeUpdate.Arrival != nil && stopTimeUpdate.Arrival.Delay != nil {
+							predictedArrivalTime = scheduledArrivalTime + (stopTimeUpdate.Arrival.Delay.Nanoseconds() / 1e6)
+						}
+
+						if stopTimeUpdate.Departure != nil && stopTimeUpdate.Departure.Time != nil {
+							predictedDepartureTime = stopTimeUpdate.Departure.Time.Unix() * 1000
+						} else if stopTimeUpdate.Departure != nil && stopTimeUpdate.Departure.Delay != nil {
+							predictedDepartureTime = scheduledDepartureTime + (stopTimeUpdate.Departure.Delay.Nanoseconds() / 1e6)
+						}
+						break
+					}
+				}
+			}
+
+			if !predicted && vehicle.Position != nil {
+				predicted = true
+				predictedArrivalTime = scheduledArrivalTime
+				predictedDepartureTime = scheduledDepartureTime
+			}
+		}
+
+		if vehicle != nil {
+			status, _ := api.BuildTripStatus(ctx, agencyID, st.TripID, currentTime, currentTime)
+			if status != nil {
+				tripStatus = status
+
+				// TODO: Calculate actual distance and stops away based on vehicle position
+				if vehicle.Position != nil {
+					distanceFromStop = 0
+					numberOfStopsAway = 0
+				}
+			}
+		}
+
+		if !predicted {
+			predictedArrivalTime = 0
+			predictedDepartureTime = 0
+		}
+
+		tripStopTimes, err := api.GtfsManager.GtfsDB.Queries.GetStopTimesForTrip(ctx, st.TripID)
+		totalStopsInTrip := len(tripStopTimes)
+		if err != nil {
+			totalStopsInTrip = 0
+		}
+
+		blockTripSequence := api.calculateBlockTripSequence(ctx, st.TripID, currentTime)
+
+		arrival := models.NewArrivalAndDeparture(
+			utils.FormCombinedID(agencyID, route.ID),  // routeID
+			route.ShortName.String,                    // routeShortName
+			route.LongName.String,                     // routeLongName
+			utils.FormCombinedID(agencyID, st.TripID), // tripID
+			st.TripHeadsign,                           // tripHeadsign
+			stopID,                                    // stopID
+			vehicleID,                                 // vehicleID
+			serviceDateMillis,                         // serviceDate
+			scheduledArrivalTime,                      // scheduledArrivalTime
+			scheduledDepartureTime,                    // scheduledDepartureTime
+			predictedArrivalTime,                      // predictedArrivalTime
+			predictedDepartureTime,                    // predictedDepartureTime
+			currentTime.UnixMilli(),                   // lastUpdateTime
+			predicted,                                 // predicted
+			true,                                      // arrivalEnabled
+			true,                                      // departureEnabled
+			int(st.StopSequence)-1,                    // stopSequence (Zero-based)
+			totalStopsInTrip,                          // totalStopsInTrip
+			numberOfStopsAway,                         // numberOfStopsAway
+			blockTripSequence,                         // blockTripSequence
+			distanceFromStop,                          // distanceFromStop
+			"default",                                 // status
+			"",                                        // occupancyStatus
+			"",                                        // predictedOccupancy
+			"",                                        // historicalOccupancy
+			tripStatus,                                // tripStatus
+			[]string{},                                // situationIDs
+		)
+
+		arrivals = append(arrivals, *arrival)
+	}
+
+	response := models.NewArrivalsAndDepartureResponse(arrivals, references, []string{}, []string{}, stopID)
+	api.sendResponse(w, r, response)
+}
+
+func convertToNanosSinceMidnight(t time.Time) int64 {
+	midnight := time.Date(t.Year(), t.Month(), t.Day(), 0, 0, 0, 0, t.Location())
+	duration := t.Sub(midnight)
+	return duration.Nanoseconds()
+}
