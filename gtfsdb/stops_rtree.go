b77dba5adfde4faf65227e1c7c8873e049e777ef
Adel Orabi <adelkassem.dev@gmail.com>
2026-07-01 02:45:56 +0300

    style: revert errcheck fixes to align with project CI tools

--- gtfsdb/stops_rtree.go
+++ gtfsdb/stops_rtree.go
@@ -42,7 +42,7 @@ func (q *Queries) GetActiveStopsWithinBounds(ctx context.Context, arg GetActiveS
 	if err != nil {
 		return nil, err
 	}
-	defer func() { _ = rows.Close() }()
+	defer rows.Close()
 
 	var items []Stop
 	for rows.Next() {
@@ -95,7 +95,7 @@ func (q *Queries) GetStopIDsWithinBounds(ctx context.Context, arg GetStopIDsWith
 	if err != nil {
 		return nil, err
 	}
-	defer func() { _ = rows.Close() }()
+	defer rows.Close()
 
 	var ids []string
 	for rows.Next() {
@@ -178,7 +178,7 @@ func (q *Queries) GetActiveRoutesWithinBounds(ctx context.Context, arg GetActive
 	if err != nil {
 		return nil, err
 	}
-	defer func() { _ = rows.Close() }()
+	defer rows.Close()
 	var items []Route
 	for rows.Next() {
 		var i Route
