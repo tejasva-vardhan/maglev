d07d0470d8785a32ede6538f7d3d0564b47d375e
Ahmedhossamdev <ahmedhossamdev1@gmail.com>
2026-07-07 03:24:33 -0700

    refactor: simplify route comparison logic by introducing routeSortKey type

--- internal/utils/sort.go
+++ internal/utils/sort.go
@@ -7,35 +7,44 @@ import (
 	"maglev.onebusaway.org/gtfsdb"
 )
 
-// compareRoutesByName compares two routes naturally by their sort name (ShortName,
+// routeSortKey holds the fields used to order routes, decoupling the comparison
+// logic from any particular gtfsdb row type.
+type routeSortKey struct {
+	shortName string
+	longName  string
+	agencyID  string
+	id        string
+}
+
+// compareRouteSortKeys compares two routes naturally by their sort name (ShortName,
 // falling back to LongName), then by AgencyID, then by ID. It is the single source
 // of truth for the ordering rules shared by the SortRoutesByName* helpers.
-func compareRoutesByName(shortNameA, longNameA, agencyIDA, idA, shortNameB, longNameB, agencyIDB, idB string) int {
-	nameA := shortNameA
+func compareRouteSortKeys(a, b routeSortKey) int {
+	nameA := a.shortName
 	if nameA == "" {
-		nameA = longNameA
+		nameA = a.longName
 	}
 
-	nameB := shortNameB
+	nameB := b.shortName
 	if nameB == "" {
-		nameB = longNameB
+		nameB = b.longName
 	}
 
 	if res := NaturalCompare(nameA, nameB); res != 0 {
 		return res
 	}
-	if agencyIDA != agencyIDB {
-		return cmp.Compare(agencyIDA, agencyIDB)
+	if a.agencyID != b.agencyID {
+		return cmp.Compare(a.agencyID, b.agencyID)
 	}
-	return cmp.Compare(idA, idB)
+	return cmp.Compare(a.id, b.id)
 }
 
 // SortRoutesByName sorts routes naturally by ShortName, falling back to LongName, then by AgencyID and ID.
 func SortRoutesByName(routes []gtfsdb.GetRoutesForStopRow) {
 	slices.SortFunc(routes, func(a, b gtfsdb.GetRoutesForStopRow) int {
-		return compareRoutesByName(
-			a.ShortName.String, a.LongName.String, a.AgencyID, a.ID,
-			b.ShortName.String, b.LongName.String, b.AgencyID, b.ID,
+		return compareRouteSortKeys(
+			routeSortKey{a.ShortName.String, a.LongName.String, a.AgencyID, a.ID},
+			routeSortKey{b.ShortName.String, b.LongName.String, b.AgencyID, b.ID},
 		)
 	})
 }
@@ -43,9 +52,9 @@ func SortRoutesByName(routes []gtfsdb.GetRoutesForStopRow) {
 // SortDBRoutesByName sorts gtfsdb.Route values using the same ordering rules as SortRoutesByName.
 func SortDBRoutesByName(routes []gtfsdb.Route) {
 	slices.SortFunc(routes, func(a, b gtfsdb.Route) int {
-		return compareRoutesByName(
-			a.ShortName.String, a.LongName.String, a.AgencyID, a.ID,
-			b.ShortName.String, b.LongName.String, b.AgencyID, b.ID,
+		return compareRouteSortKeys(
+			routeSortKey{a.ShortName.String, a.LongName.String, a.AgencyID, a.ID},
+			routeSortKey{b.ShortName.String, b.LongName.String, b.AgencyID, b.ID},
 		)
 	})
 }
